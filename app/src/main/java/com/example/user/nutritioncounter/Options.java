8e9f6ebce0df61d511a48b17f57e0b9718d22da6
YANAY1ALON <yanay1alon@gmail.com>
2018-05-10 10:34:21 +0300

    Options.java recreated

--- app/src/main/java/com/example/user/nutritioncounter/Options.java
+++ app/src/main/java/com/example/user/nutritioncounter/Options.java
@@ -2,12 +2,36 @@ package com.example.user.nutritioncounter;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.WindowManager;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 
 public class Options extends AppCompatActivity {
+
+    boolean main = true, db = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_options);
+        getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_ADJUST_PAN);
+
     }
+
+    public void buttonchk(View view) {
+        RadioButton r = (RadioButton) view;
+        if (main) {
+            int choices = 0;
+            if (r.isChecked()){
+                r.clearFocus();
+                choices--;
+            }
+            if (choices > 3) {
+                r.clearFocus();
+            }
+        }
+
+    }
+
 }
